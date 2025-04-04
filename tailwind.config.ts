185f982e93f11e4875f41da004c617542f307ca5
aliceresende <97093442+aliceresende@users.noreply.github.com>
2025-04-04 16:36:02 +0100

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,9 @@ import type { Config } from "tailwindcss";
 export default {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{ts,tsx}",
-		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
-		"./src/**/*.{ts,tsx}",
-	],
+		"./src/**/*.{ts,tsx,js,jsx}",
+		"./index.html",
+	  ],
 	prefix: "",
 	theme: {
 		container: {
