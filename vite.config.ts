8bc9814c75ea03eb92b2381bb8c3bbbb6cb79fce
aliceresende <97093442+aliceresende@users.noreply.github.com>
2025-04-04 16:11:11 +0100

    .

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 export default defineConfig(({ mode }) => ({
-  base: "/gwhite/",  // 👈 Set the correct base path
+  base: "/gwhite/",  
   server: {
     host: "::",
     port: 8080,
